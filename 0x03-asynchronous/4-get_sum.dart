00a33ddd0b9ed6cb0fe8c458e266b038327ef3ff
nuocmam75009 <sayhi.kiseiju@gmail.com>
2026-04-03 11:14:53 +0200

    d

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import '4-util.dart';
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
   try {
     String userData = await fetchUserData();
     Map<String, dynamic> user = jsonDecode(userData);
